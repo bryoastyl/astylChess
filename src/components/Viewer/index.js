9811c00e501782f8d44c84f660fc7f0c31ed9715
ronjones340 <ej07696@gmail.com>
2022-04-02 23:19:09 +0300

    Time has been fixed waiting to be connected to the board

--- src/components/Viewer/index.js
+++ src/components/Viewer/index.js
@@ -1,32 +1,17 @@
-<<<<<<< HEAD
-import React from 'react'
-import css from './Viewer.module.css'
-import ConnectBoard from '../Board'
-import DataBoard from '../Databoard'
-import Chat from '../Chat'
-
-function index() {
-    return (
-        <div className={css.Viewer}>
-            <DataBoard />
-            <ConnectBoard />
-            <Chat />
-        </div>
-    )
-=======
 import React from "react";
 import css from "./Viewer.module.css";
 import ConnectBoard from "../Board";
 import DataBoard from "../Databoard";
+import Chats from "../Chat"
 
 function index() {
   return (
     <div className={css.Viewer}>
       <DataBoard mins="25" />
       <ConnectBoard />
+      <Chats />
     </div>
   );
->>>>>>> c87eff14b43ea687584c2f54dd249d0fb977ed9c
 }
 
 export default index;
